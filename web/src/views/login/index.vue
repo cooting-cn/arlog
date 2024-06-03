d86c442379b15fc1254a1b3153cef4cfbd053a73
cooting <cooting-cn@qq.com>
2024-06-04 00:31:58 +0800

    登录页创建

--- web/src/views/login/index.vue
+++ web/src/views/login/index.vue
@@ -3,9 +3,68 @@
 </script>
 
 <template>
-<h1>这是登录页面</h1>
+  <!--整个页面布局-->
+  <div class="container">
+    <!--登录框    -->
+    <el-card class="login">
+
+      <!--    显示头文字  -->
+      <div style="text-align: center;">
+        <div class="flex-item">
+          <!--      版本标签    -->
+          <el-tag type="primary">latest</el-tag>
+        </div>
+        <h1 style="margin-top: 10px">arlog登录</h1>
+      </div>
+
+      <!--    表单用户 密码  -->
+      <!--   用户   -->
+      <div class="sr">
+        <el-input v-model="input" class="custom-input" placeholder="Please input"/>
+      </div>
+      <!--    密码  -->
+      <div class="sr">
+        <el-input v-model="input" class="custom-input" placeholder="Please input" size="large"/>
+      </div>
+
+      <!--  三方登录    -->
+      <template #footer>三方登录</template>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
 
+.custom-input {
+
+  height: 45px; /* 自定义高度 */
+  font-size: 16px; /* 自定义字体大小 */
+}
+
+.sr {
+  margin-top: 20px;
+
+}
+
+.flex-item {
+  position: absolute;
+  top: 15px;
+  right: 15%;
+  padding: 10px;
+}
+
+/*登录框居中*/
+.login {
+  margin: 0 auto; /* 水平居中 */
+  position: relative; /* 相对定位 */
+  top: 20%;
+  max-width: 400px;
+
+  border-radius: 20px; /*弧度角度*/
+}
+
+/*容器布局高度100%*/
+.container {
+  height: 100%;
+}
 </style>
