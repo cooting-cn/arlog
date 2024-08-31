98eb4141f590c7a2af586a5de916804037f1d2be
cooting <cooting-cn@qq.com>
2024-08-31 13:47:58 +0800

    1

--- web-admin/src/layout/admin.vue
+++ web-admin/src/layout/admin.vue
@@ -1,26 +1,51 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <n-layout class="h-80%" has-sider>
-    <n-layout-sider>
-      菜单
-    </n-layout-sider>
+  <!--  <n-layout class="h-80%" has-sider>
+      <n-layout-sider>
+        菜单
+      </n-layout-sider>
 
-    <n-layout>
-      <n-layout-header class="h-5% ">导航</n-layout-header>
+      <n-layout>
+        <n-layout-header class="h-5% ">导航</n-layout-header>
 
-      <n-layout-content class="h-50%" content-style="padding: 24px;">
-        内容
-        <!--加载路由-->
-        <RouterView/>
+        <n-layout-content class="h-50%" content-style="padding: 24px;">
+          内容
+          &lt;!&ndash;加载路由&ndash;&gt;
+          <RouterView/>
 
-      </n-layout-content>
+        </n-layout-content>
 
-      <n-layout-footer class="h-5%">底下导航</n-layout-footer>
+        <n-layout-footer class="h-5%">底下导航</n-layout-footer>
 
-    </n-layout>
+      </n-layout>
 
-  </n-layout>
+    </n-layout>-->
+
+  <div class="h-full flex">
+    <aside
+        border-r="1px solid light_border dark:dark_border"
+        class="flex-col flex-shrink-0 transition-width-300 w-220 "
+    >
+      <h1>菜单</h1>
+    </aside>
+
+    <article class="w-0 flex-col flex-1">
+      <div class="h-5% min-h-5%">
+        <h1>导航</h1>
+      </div>
+
+      <div class="h-95% min-h-95%">
+        <h1>
+          内容
+          <router-view></router-view>
+        </h1>
+      </div>
+
+
+    </article>
+  </div>
 
 </template>
 
